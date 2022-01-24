79f51146edd7c93499d4bb76a9e6312d7811658e
Nadia Posch <nadia@smartive.ch>
2022-01-24 13:38:48 +0100

    update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  content: [
-    './src/components/**/*.{ts,tsx,js,jsx}', 
-    './src/pages/**/*.{ts,tsx,js,jsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    content: ["./src/components/**/*.{ts,tsx,js,jsx}", "./src/pages/**/*.{ts,tsx,js,jsx}"],
+    theme: {
+        extend: {
+            keyframes: {
+                growDown: {
+                    from: {
+                        transform: "translateY(-10px)",
+                        opacity: 0,
+                    },
+
+                    to: {
+                        transform: "translateY(0)",
+                        opacity: "100%",
+                    },
+                },
+            },
+            animation: {
+                growDown: "growDown 300ms ease-in-out",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
