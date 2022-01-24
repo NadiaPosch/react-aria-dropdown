0a6bf639f3adbbd26d4fefe172dbf244384501dd
Nadia Posch <nadia@smartive.ch>
2022-01-24 13:39:29 +0100

    add types

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+import { AriaListBoxOptions } from "@react-aria/listbox";
+import { ListState } from "@react-stately/list";
+import { Node } from "@react-types/shared";
+import { PropsWithChildren } from "react";
+
+export type MenuItemType = {
+    id: string | number;
+    title: string;
+};
+
+export type MenuBlock = {
+    id: string;
+    menuItems: MenuItemType[];
+    ariaLabel?: string;
+};
+
+export type DropdownProps = {
+    menuBlocks: MenuBlock[];
+    onChange: (id?: string | number) => void;
+    activeItemId?: string | number;
+    ariaLabel?: string;
+};
+
+export type SelectMenuProps = {
+    menuBlocks: MenuBlock[];
+    ariaProps: AriaListBoxOptions<any>;
+    state: ListState<any>;
+    ariaLabel?: string;
+};
+
+export type MenuSectionProps = PropsWithChildren<{
+    ariaLabel?: string;
+}>;
+
+export type MenuItemProps = {
+    state: ListState<object>;
+    node: Node<object>;
+    title: string;
+};
