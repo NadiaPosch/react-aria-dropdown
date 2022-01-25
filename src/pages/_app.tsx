43cfd53c1ad1ac93cbcc5b4ad6ea84069ede4fcf
Nadia Posch <nadia@smartive.ch>
2022-01-25 17:22:06 +0100

    add SSR provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
+import { SSRProvider } from "@react-aria/ssr";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <SSRProvider>
             <Head>
                 <title>React Aria Dropdown Example</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <Component {...pageProps} />
-        </>
+        </SSRProvider>
     );
 }
 
