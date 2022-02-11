1ddd3e8ef7ccb3e7115bd50e5b5d5e8ce408a15e
Nadia Posch <nadia@smartive.ch>
2022-02-11 09:35:23 +0100

    refactor Switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,14 +1,46 @@
-import { FC, useState } from "react";
+import { useFocusRing } from "@react-aria/focus";
+import { useSwitch } from "@react-aria/switch";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
+import { useToggleState } from "@react-stately/toggle";
+import { FC, useRef } from "react";
 
-export const Switch: FC = ({ children }) => {
-    const [active, setActive] = useState(false);
+type SwitchProps = {
+    isSelected?: boolean;
+    onChange?: (isSelected: boolean) => void;
+};
+
+export const Switch: FC<SwitchProps> = ({ children, isSelected = false, onChange }) => (
+    <label className="flex items-center gap-x-4" onClick={() => onChange(!isSelected)}>
+        <div className={`cursor-pointer border border-gray-400 rounded-full w-10 outline-none`}>
+            <div
+                className={`block self-center w-5 h-5 rounded-full transition-transform duration-300 ${
+                    isSelected ? "translate-x-full bg-blue-500" : "translate-x-0 bg-gray-400"
+                }`}
+            />
+        </div>
+        {children}
+    </label>
+);
+
+export const AccessibleSwitch: FC<SwitchProps> = ({ children, isSelected = false, onChange }) => {
+    const state = useToggleState({ isSelected, onChange });
+    const ref = useRef();
+    const { inputProps } = useSwitch({}, state, ref);
+    const { isFocusVisible, focusProps } = useFocusRing();
 
     return (
-        <label className="flex items-center gap-x-4" onClick={() => setActive(!active)}>
-            <div className={`cursor-pointer border border-gray-400 rounded-full w-10 outline-none`}>
+        <label className="flex items-center gap-x-4">
+            <VisuallyHidden>
+                <input {...inputProps} {...focusProps} ref={ref} />
+            </VisuallyHidden>
+            <div
+                className={`cursor-pointer border border-gray-400 rounded-full w-10 outline-none ${
+                    isFocusVisible ? "ring-4 ring-blue ring-offset-2" : ""
+                }`}
+            >
                 <div
                     className={`block self-center w-5 h-5 rounded-full transition-transform duration-300 ${
-                        active ? "translate-x-full bg-blue-500" : "translate-x-0 bg-gray-400"
+                        isSelected ? "translate-x-full bg-blue-500" : "translate-x-0 bg-gray-400"
                     }`}
                 />
             </div>
